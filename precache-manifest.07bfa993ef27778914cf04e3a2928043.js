145c01b754432c8459dc3b9c5c893c709ef16df1
Aliaksandr Rusak <rusak.alexander2017@gmail.com>
2020-01-26 20:58:43 +0300

    Updates

---
+++ precache-manifest.07bfa993ef27778914cf04e3a2928043.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "177bf4480931ea725989b4985a1eb450",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "a08f1b72f2b9ace5477f",
+    "url": "/my-app/static/css/main.5e3b0277.chunk.css"
+  },
+  {
+    "revision": "29285559024cec2c9e00",
+    "url": "/my-app/static/js/2.adc1112f.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/my-app/static/js/2.adc1112f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a08f1b72f2b9ace5477f",
+    "url": "/my-app/static/js/main.b98da47a.chunk.js"
+  },
+  {
+    "revision": "134c6d7b033e4b816dc9",
+    "url": "/my-app/static/js/runtime-main.56c001b7.js"
+  }
+]);
